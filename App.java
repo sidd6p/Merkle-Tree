a781c1386bde6916995d882c41513358958c9d47
Siddhartha <siddpurwar@gmail.com>
2022-04-27 23:29:30 +0530

    updated Hashing Algorithm

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws NoSuchAlgorithmException {
         // Data to add at leaf of the Merkle Tree
-        List<String> data = Arrays.asList("Si", "Gurur", "Java", "VIT");
+        List<String> data = Arrays.asList("Siddhartha", "Gurur", "Java", "VIT");
 
         // Making Merkle Tree from the data
         Node merkleTreeRoot = MerkleTree.makeTree(data);
--- HashAlgo.java
+++ HashAlgo.java
@@ -1,23 +1,30 @@
 // A Java program to find the SHA-256 hash value  
 
-import java.security.NoSuchAlgorithmException;  
-import java.math.BigInteger;  
-import java.security.MessageDigest;  
-import java.nio.charset.StandardCharsets;  
-  
-public class HashAlgo {  
-    public static byte[] obtainSHA(String s) throws NoSuchAlgorithmException  {  
-        MessageDigest msgDgst = MessageDigest.getInstance("SHA-256");  
-        return msgDgst.digest(s.getBytes(StandardCharsets.UTF_8));  
-    }  
-    
-    public static String getHash(String data) throws NoSuchAlgorithmException {  
-        byte[] hash = obtainSHA(data);
-        BigInteger no = new BigInteger(1, hash);  
-        StringBuilder hexStr = new StringBuilder(no.toString(16));  
-        while (hexStr.length() < 32) {  
-            hexStr.insert(0, '0');  
-        }  
-        return hexStr.toString();  
-    }    
-}  
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashAlgo {
+
+   public String getHash(final String input) throws NoSuchAlgorithmException {
+
+      String hashtext = null;
+      MessageDigest md = MessageDigest.getInstance("MD5");
+
+      // Compute message digest of the input
+      byte[] messageDigest = md.digest(input.getBytes());
+
+      hashtext = convertToHex(messageDigest);
+
+      return hashtext;
+   }
+
+   private String convertToHex(final byte[] messageDigest) {
+      BigInteger bigint = new BigInteger(1, messageDigest);
+      String hexText = bigint.toString(16);
+      while (hexText.length() < 32) {
+         hexText = "0".concat(hexText);
+      }
+      return hexText;
+   }
+}
